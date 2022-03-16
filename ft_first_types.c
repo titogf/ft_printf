8fb7b33b1333861cd5b93aefcf4fa4355d9ee144
Gon Fa <gonfa@MacBook-Pro-de-Gon.local>
2022-03-16 20:17:32 +0100

    2

--- ft_first_types.c
+++ ft_first_types.c
@@ -27,35 +27,5 @@ int	ft_type_s(char *str)
 
 int	ft_type_di(int	n)
 {
-	int	a;
-	int	result;
 
-	result = 0;
-	if (n == -2147483648)
-	{
-		write (1, "-2147483648", 11);
-		return (11);
-	}
-	if (n < 0)
-	{
-		write(1, "-", 1);
-		n *= -1;
-		result++;
-	}
-	if (n < 10)
-	{
-		n += 48;
-		write(1, &n, 1);
-		result++;
-		return (result);
-	}
-	else
-	{
-		a = (n % 10) + 48;
-		n /= 10;
-		ft_type_di(n);
-		write (1, &a, 1);
-		result++;
-	}
-	return (result);
 }
