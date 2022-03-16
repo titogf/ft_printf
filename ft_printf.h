a49139998cebcb9f752385d312d3be6f288ee40b
Gon Fa <gonfa@MacBook-Pro-de-Gon.local>
2022-03-16 13:10:20 +0100

    2

--- ft_printf.h
+++ ft_printf.h
@@ -5,6 +5,9 @@
 
 int	ft_printf(const char *str, ...);
 int	ft_return_arg(const char *str, va_list lst);
+int	ft_type_NULL(void);
+int	ft_type_c(char str);
 int	ft_type_s(char *str);
+int	ft_type_di(int n);
 
 #endif
