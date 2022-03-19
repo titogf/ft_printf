7feb98a539b861da4876a04e6348569d239a185d
Gon Fa <gonfa@MacBook-Pro-de-Gon.local>
2022-03-19 21:56:25 +0100

    5

--- prueba.c
+++ prueba.c
@@ -25,25 +25,8 @@ int	ft_type_x(unsigned int n)
 
 	longi = ft_long(n);	
 	s = malloc(sizeof(char) * (longi + 1));
-	a = (n % 16) + 48;
-	if (a == 10 || a == 11 || a == 12 || a == 13 || a == 14 || a == 15)
-		{
-			if (a == 10)
-				s[i] = 'a';
-			if (a == 11)
-				s[i] = 'b';
-			if (a == 12)
-				s[i] = 'c';
-			if (a == 13)
-				s[i] = 'd';
-			if (a == 14)
-				s[i] = 'e';
-			if (a == 15)
-				s[i] = 'f';
-		}
-		else
-			s[i] = (n % 10) + 48;
 	i = 0;
+	b = (n % 16) + 48;
 	while (n > 15)
 	{
 		a = n / 16;
@@ -67,7 +50,23 @@ int	ft_type_x(unsigned int n)
 		n /= 16;
 		i++;
 	}
-	s[i] = (b % 16) + 48;
+	if (b == 10 || b == 11 || b == 12 || b == 13 || b == 14 || b == 15)
+		{
+			if (b == 10)
+				s[i] = 'a';
+			if (b == 11)
+				s[i] = 'b';
+			if (b == 12)
+				s[i] = 'c';
+			if (b == 13)
+				s[i] = 'd';
+			if (b == 14)
+				s[i] = 'e';
+			if (b == 15)
+				s[i] = 'f';
+		}
+		else
+			s[i] = b;
 	s[longi] = '\0';
 	i = ft_type_s(s);
 	free(s);
